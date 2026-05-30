804e4058ccdd2d2d295da2ab7c582c019de7d80b
Anunay Pandey <anunaypandey115@gmail.com>
2026-05-30 20:21:02 +0530

    user input output in python
    Added a Python script for input and output demonstration.

---
+++ concepts/day1/inputoutput.py
@@ -0,0 +1,7 @@
+#input is used to get data from the user. In Python, we can use the input() function to get input from the user. The input() function takes a string as an argument, which is displayed to the user as a prompt.
+#output is used to display data to the user. In Python, we can use the print() function to display output to the user. The print() function takes one or more arguments, which are the values that we want to display.
+
+name = input("Enter your name: ") #getting input from the user
+#here we are using the input() function to get the user's name and storing it in the variable name.
+print("Hello, " + name + "!") #displaying output to the user
+# "Hello, " + name + "!" this is basically called concatenation of strings will discuss them briefly
