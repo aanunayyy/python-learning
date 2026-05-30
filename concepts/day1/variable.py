51c9802231d23b165d616a6921f8692ae433468d
Anunay Pandey <anunaypandey115@gmail.com>
2026-05-30 20:10:05 +0530

    Basic concepts of Python Variables

---
+++ concepts/day1/variable.py
@@ -0,0 +1,23 @@
+#variables are used to store data in a program. They can be of different data types such as integers, floats, strings, and booleans.
+a = 10 #here a is a variable
+#it holds a value of integer 10
+
+#There are certain rule for naming a variable:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+#camel case is the most common format for naming in a variable in python (myNamAnunay)
+#assigning multiple values to variables 
+x,y,z = "orange", 1, "anunay"
+print(x) #orange
+print(y) #1
+print(z) #anunay
+#assigning single value to variables
+x = y = z = "anunay"
+print(x) #anunay
+print(y) #anunay
+print(z) #anunay
+
