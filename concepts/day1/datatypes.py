ea69ed1b11871fd03294a90cbd0d0a987955db37
Anunay Pandey <anunaypandey115@gmail.com>
2026-05-30 20:16:24 +0530

    Datatypes Notes
    Added a Python script to demonstrate various data types including integer, float, string, and boolean, along with comments explaining each type.

---
+++ concepts/day1/datatypes.py
@@ -0,0 +1,16 @@
+#data types are used to define the type of data that a variable can hold. For example, an integer variable can only hold whole numbers, while a string variable can hold text.
+a = 10 #integer variable
+#here a is a variable that holds the value 10, which is an integer data type.
+b = 3.14 #float variable
+#here b is a variable that holds the value 3.14, which is a float data type.
+c = "Hello, World!" #string variable
+#here c is a variable that holds the value "Hello, World!", which is a string data type.
+d = True #boolean variable
+#here d is a variable that holds the value True, which is a boolean data type.
+type(a) #checking the data type of variable a
+type(b) #checking the data type of variable b
+type(c) #checking the data type of variable c
+type(d) #checking the data type of variable d
+#here we are using the type() function to check the data type of variable a, which will return <class 'int'> since a is an integer variable.
+
+#there are many datatypes such as list, dict, tuple don't woryy i will be covering them very soon.
